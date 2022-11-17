2e346639c77df35cae04eef36fce190498f1cb6c
Neilzon <neilcviloria@gmail.com>
2022-11-17 01:43:36 -0700

    Setup apollo iOS client

---
+++ WorkoutLoggerAPI/Sources/Unions/AuthResult.graphql.swift
@@ -0,0 +1,14 @@
+// @generated
+// This file was automatically generated and should not be edited.
+
+import ApolloAPI
+
+public extension Unions {
+  static let AuthResult = Union(
+    name: "AuthResult",
+    possibleTypes: [
+      Objects.AuthError.self,
+      Objects.AuthSuccess.self
+    ]
+  )
+}
