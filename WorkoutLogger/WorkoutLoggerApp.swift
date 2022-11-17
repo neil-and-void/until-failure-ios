6aff4b2ad1ed912a73ef4f68f7fb6862d7e927b0
Neilzon <neilcviloria@gmail.com>
2022-11-17 01:22:00 -0700

    Initial Commit

---
+++ WorkoutLogger/WorkoutLoggerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutLoggerApp.swift
+//  WorkoutLogger
+//
+//  Created by Neil Viloria on 2022-11-17.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutLoggerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
