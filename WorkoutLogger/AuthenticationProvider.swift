2f4cada199c24a73faa5933d826e50411244c7b4
Neilzon <neilcviloria@gmail.com>
2022-12-05 01:51:26 -0700

    clear cache on logout

--- WorkoutLogger/AuthenticationProvider.swift
+++ WorkoutLogger/AuthenticationProvider.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import Apollo
 
 protocol AuthenticationStateProtocol {
     func setAuth(_ isAuthenticated: Bool)
@@ -13,9 +14,16 @@ protocol AuthenticationStateProtocol {
 
 // global state of whether user is signed in or not
 class AuthenticationState: ObservableObject, AuthenticationStateProtocol {
+    private var client: ApolloClientProtocol
     @Published var isAuthenticated = false
     
+    init(client: ApolloClientProtocol = WorkoutLoggerAPIClient.client) {
+        self.client = client
+    }
+    
     func setAuth(_ isAuthenticated: Bool) {
-        self.isAuthenticated = isAuthenticated
+        self.client.store.clearCache() { _ in
+            self.isAuthenticated = isAuthenticated
+        }
     }
 }
