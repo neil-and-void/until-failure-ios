3c7e195d2caca9349a2bed3f84baaac08a197863
Neilzon <neilcviloria@gmail.com>
2022-12-24 12:30:51 -0700

    Layout for editable exercise

--- WorkoutLogger/Features/Exercises/Edit/EditableExercise.swift
+++ WorkoutLogger/Features/Exercises/Edit/EditableExercise.swift
@@ -12,36 +12,54 @@ struct EditableExercise: View {
     
     var body: some View {
         
-        GeometryReader { geometry in
+        VStack {
             
-            VStack(alignment: .leading) {
+            HStack {
                 
                 VStack(alignment: .leading) {
                     
-                    Text("Squat")
+                    Text(exercise.exerciseRoutine.name)
                         .font(.system(size: 24, weight: .semibold))
                     
-                    Text("\(String(4)) sets, \(6) reps")
+                    Text("\(String(exercise.exerciseRoutine.sets)) sets, \(String(exercise.exerciseRoutine.reps)) reps")
                         .foregroundColor(.secondaryText)
                     
                 }
                 
-                Divider()
+                Spacer()
                 
+            }
+            
+            Divider()
+            
+            if exercise.sets.count == 0 {
+                
+                Text("No set entries yet")
+                    .foregroundColor(.tertiaryText)
+                    .padding(.vertical, 2)
+                    .padding(.horizontal, 2)
+                
+            } else {
                 
                 Grid {
                     
                     GridRow {
                         
-                        Text("Sets")
+                        Text("Set")
+                            .fontWeight(.semibold)
                         
                         Text("Prev")
+                            .fontWeight(.semibold)
                         
                         Text("Reps")
+                            .fontWeight(.semibold)
                         
-                        Text("Sets")
+                        Text("Weight")
+                            .fontWeight(.semibold)
                         
                     }
+                    
+                    
                     
                     ForEach($exercise.sets) { setEntry in
                         
@@ -51,11 +69,12 @@ struct EditableExercise: View {
                                 .fontWeight(.semibold)
                             
                             Text(String("225 lbs x 4 reps"))
-                                .frame(width: .infinity)
+                                .frame(maxWidth: .infinity)
                                 .padding(4)
                                 .lineLimit(1)
                                 .multilineTextAlignment(.center)
                                 .background(.thinMaterial)
+                                .foregroundColor(.secondaryText)
                                 .cornerRadius(8)
                             
                             TextField("reps", value: setEntry.reps, formatter: NumberFormatter())
@@ -74,21 +93,29 @@ struct EditableExercise: View {
                             
                         }
                         
-                    }
-                    
-                    Button {
                         
-                        print("flksdjf")
-                        
-                    } label: {
-                        
-                        Text("+ Add Set")
                         
                     }
                     
-                }.frame(width: geometry.size.width)
+
+                    
+                }.frame(maxWidth: .infinity)
                 
-                Divider()
+            }
+            
+            Button {
+                
+                print("flksdjf")
+                
+            } label: {
+                
+                Text("+ Add Set")
+                
+            }.buttonStyle(TextButton())
+
+            Divider()
+            
+            VStack(alignment: .leading) {
                 
                 Text("Notes")
                     .font(.system(size: 20, weight: .semibold))
@@ -100,29 +127,37 @@ struct EditableExercise: View {
                     .cornerRadius(8)
                 
             }
-            .padding(10)
-            .background(Color.bgSecondary)
-            .cornerRadius(10)
             
         }
-        
-        
+        .padding(10)
+        .background(Color.bgSecondary)
+        .cornerRadius(10)
         
     }
+    
 }
 
 struct EditableExercise_Previews: PreviewProvider {
     static var previews: some View {
-        EditableExercise(exercise: .constant(Exercise(
-            id: "1",
-            exerciseRoutine: EmbeddedExerciseRoutine(id: "1", name: "Squat", sets: 4, reps: 5),
-            sets: [
-                SetEntry(id: "1", weight: 225, reps: 4),
-                SetEntry(id: "2", weight: 225, reps: 4),
-                SetEntry(id: "3", weight: 225, reps: 4),
-                SetEntry(id: "4", weight: 225, reps: 4),
-            ],
-            notes: "Somwething"))
-        ).preferredColorScheme(.dark)
+        
+        ScrollView {
+            EditableExercise(exercise: .constant(Exercise(
+                id: "1",
+                exerciseRoutine: EmbeddedExerciseRoutine(id: "1", name: "Squat", sets: 4, reps: 5),
+                sets: [
+                    SetEntry(id: "1", weight: 225, reps: 4),
+                    SetEntry(id: "2", weight: 225, reps: 4),
+                    SetEntry(id: "3", weight: 225, reps: 4),
+                    SetEntry(id: "4", weight: 225, reps: 4),
+                ],
+                notes: "Somwething"))
+            )
+            EditableExercise(exercise: .constant(Exercise(
+                id: "1",
+                exerciseRoutine: EmbeddedExerciseRoutine(id: "1", name: "Squat", sets: 4, reps: 5),
+                sets: [],
+                notes: "Somwething"))
+            )
+        }.preferredColorScheme(.dark)
     }
 }
