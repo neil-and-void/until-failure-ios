6033390494cf38aaf4cad4b92d33fdd3541e97b3
Neilzon <neilcviloria@gmail.com>
2023-03-19 13:49:08 -0600

    Update config files

--- WorkoutLogger/Config.swift
+++ WorkoutLogger/Config.swift
@@ -9,13 +9,13 @@ import Foundation
 
 var baseURL: String {
     #if DEV
-    return "http://localhost:8080"
-//    return "https://workout-logger-api-ejtky726bq-uw.a.run.app"
+//    return "http://localhost:8080"
+    return "https://workout-logger-api-ejtky726bq-uw.a.run.app"
     #elseif STAGING
-    return "http://localhost:8080"
-//    return "https://workout-logger-api-ejtky726bq-uw.a.run.app"
+//    return "http://localhost:8080"
+    return "https://workout-logger-api-ejtky726bq-uw.a.run.app"
     #else
-    return "http://localhost:8080"
-//    return "https://workout-logger-api-ejtky726bq-uw.a.run.app"
+//    return "http://localhost:8080"
+    return "https://workout-logger-api-ejtky726bq-uw.a.run.app"
     #endif
 }
